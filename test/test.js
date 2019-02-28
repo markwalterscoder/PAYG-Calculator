f07637710280a12898c00faf41d46ef6cd73c21a
Mark Walters <markwalterscoder@gmail.com>
2019-03-01 00:33:01 +1100

    Add linting

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
-const should = require('chai').should()
+const mocha = require('mocha')
+const describe = mocha.describe
+const it = mocha.it
 
-describe('Database Tests', ()=>{
-    it('Test Stub', ()=> {   
+describe('Scale Tests', () => {
+  describe('2018-2019 Scales', () => {
+    it('Scale 1 should be correct', () => {
+
     })
+  })
 })
